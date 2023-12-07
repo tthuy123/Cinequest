e747777e6ef50ce6ca80b38ab3a783288f7c8772
thuthuy <125456003+tthuy123@users.noreply.github.com>
2023-12-07 12:46:00 +0700

    sign out

--- public/auth.js
+++ public/auth.js
@@ -1,11 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('loginForm').addEventListener('submit', function (event) {
-      // Prevent the default form submission behavior
-      event.preventDefault();
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('loginForm').addEventListener('submit', function (event) {
+//       // Prevent the default form submission behavior
+//       event.preventDefault();
 
-      // Call the login function when the form is submitted
-      login();
-    });
+//       // Call the login function when the form is submitted
+//       login();
+//     });
+//   });
+
+//   function login() {
+//     const userName = document.getElementById('usernameInput').value;
+//     const password = document.getElementById('passwordInput').value;
+
+//     // Perform the login request to your backend here
+//     fetch('http://localhost:3000/login', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ userName, password }),
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         // Check if login was successful
+//         if (data.token) {
+//             localStorage.setItem('authToken', data.token);
+//             console.log(data.token);
+//           // Display the logged-in user's name in an alert
+//           alert(`Welcome, ${data.user.userName}!`);
+
+//           // You can also update the UI, for example, hide the login form
+//           document.getElementById('loginForm').style.display = 'none';
+//         } else {
+//           // Handle login failure, show an error message or take appropriate action
+//           console.error('Login failed:', data.message);
+//         }
+//       })
+//       .catch(error => console.error('Error:', error));
+//   }
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  const loginForm = document.getElementById('loginForm');
+
+  // Check if the user is already signed in
+ // const authToken = localStorage.getItem('authToken');
+ // updateSignInStatus(authToken);
+
+  // Add an event listener to the login form
+  loginForm.addEventListener('submit', function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Call the login function when the form is submitted
+    login();
   });
 
   function login() {
@@ -24,13 +72,15 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(data => {
         // Check if login was successful
         if (data.token) {
-            localStorage.setItem('authToken', data.token);
-            console.log(data.token);
+          localStorage.setItem('authToken', data.token);
+
           // Display the logged-in user's name in an alert
           alert(`Welcome, ${data.user.userName}!`);
 
-          // You can also update the UI, for example, hide the login form
-          document.getElementById('loginForm').style.display = 'none';
+          // Update the "Sign In" link to "Sign Out"
+          updateSignInStatus(data.token);
+           document.getElementById('loginForm').style.display = 'none';
+
         } else {
           // Handle login failure, show an error message or take appropriate action
           console.error('Login failed:', data.message);
@@ -38,3 +88,33 @@ document.addEventListener('DOMContentLoaded', function () {
       })
       .catch(error => console.error('Error:', error));
   }
+
+  function updateSignInStatus(token) {
+    const signinButton = document.getElementById('signinButton');
+    const signinText = document.getElementById('signinText');
+  
+    if (token !== null) {
+      // User is signed in, update the link to "Sign Out"
+      signinText.textContent = 'Sign Out';
+      signinButton.addEventListener('click', signOut);
+    } else {
+      // User is not signed in, update the link to "Sign In"
+      signinText.textContent = 'Sign In';
+      signinButton.addEventListener('click', login);
+    }
+  }
+  
+
+  function signOut() {
+    // Perform the sign-out logic, e.g., remove the token from local storage
+    localStorage.removeItem('authToken');
+
+    // Update the "Sign In" link after signing out
+    updateSignInStatus(null);
+  }
+
+  function signIn() {
+    // Handle any custom logic for signing in, if needed
+    // ...
+  }
+});
